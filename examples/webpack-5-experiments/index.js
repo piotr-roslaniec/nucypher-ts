fb6bd67ca36ee71eccbdd77b55475279c12e9a35
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-07-27 10:57:03 +0200

    modify the original example

--- examples/webpack-5-experiments/index.js
+++ examples/webpack-5-experiments/index.js
@@ -1,4 +1,4 @@
-import { Alice, Bob, SecretKey } from '@nucypher/nucypher-ts';
+import { Alice, Bob, SecretKey, Enrico } from '@nucypher/nucypher-ts';
 import { ethers } from 'ethers';
 
 const txtEncoder = new TextEncoder();
@@ -18,51 +18,53 @@ const makeBob = () => {
   return Bob.fromSecretKey(config, secretKey);
 };
 
-const makeRemoteBob = () => {
-  // The difference between a "Bob" and a "remote Bob" is that we only have
-  // access to public parameters in the latter, whereas in the former
-  // we also have access to Bob's secret key
-  const { decryptingKey, verifyingKey } = makeBob();
-  return { decryptingKey, verifyingKey };
-};
-
-const getRandomLabel = () => `label-${new Date().getTime()}`;
-
 const runExample = async () => {
   if (!window.ethereum) {
     console.error('You need to connect to the MetaMask extension');
   }
 
-  alert('Sign a transaction to create a policy.');
-
   const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
   await provider.send('eth_requestAccounts', []);
 
-  const remoteBob = makeRemoteBob();
-  const threshold = 2;
-  const shares = 3
+  // first create the encrypted message
+  const label = 'Some Label'
+  const plainTextMessage = 'Hello World'
+  const alice = makeAlice(provider);
+  const policyEncryptingKeyFromLabel = alice.getPolicyEncryptingKeyFromLabel(label)
+  const enrico = new Enrico(policyEncryptingKeyFromLabel)
+  const messageKit = enrico.encryptMessage(plainTextMessage)
+
+  // meanwhile there is a Bob
+  const bob = makeBob();
+  const bobPublicKey = bob.verifyingKey
+
+  // and bob gives his public key to alice
+  const remoteBob = { verifyingKey: bobPublicKey, decryptingKey: bobPublicKey }
+
+  // back to alice - she is now creating a policy
+  const threshold = 1
+  const shares = 1
   const startDate = new Date()
   const endDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30) // In 30 days
-  const policyParams = {
-    bob: remoteBob,
-    label: getRandomLabel(),
-    threshold,
-    shares,
-    startDate,
-    endDate,
-  }
+  const policyParams = { bob: remoteBob, label, threshold, shares, startDate, endDate }
+  // We can just do Alice::grant to combine there steps:
+  const rawPolicy = await alice.generatePreEnactedPolicy(policyParams)
+  const policy = await rawPolicy.enact(alice)
 
-  const alice = makeAlice(provider);
-  const includeUrsulas = [];
-  const excludeUrsulas = [];
-  const policy = await alice.grant(
-    policyParams,
-    includeUrsulas,
-    excludeUrsulas
-  );
+  //The following info is made available to bob
+  const policyEncryptingKey = policy.policyKey
+  const aliceVerifyingKey = alice.verifyingKey
+  const encryptedTreasureMap = policy.encryptedTreasureMap
+  // as well as the messageKit
 
-  console.log('Policy created:');
-  console.log({ policy });
+  //back to bob - he is retrieving and decrypting
+
+  const retrievedMessage = await bob.retrieveAndDecrypt(
+    policyEncryptingKey,
+    aliceVerifyingKey,
+    [messageKit],
+    encryptedTreasureMap
+  )
 };
 
 runExample();
